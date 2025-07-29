3d1bbe1e8b93cd98cb1a6bc581b29ffdbe661e13
mariotg2003 <mariotirado2003@gmail.com>
2025-07-29 20:09:10 +0200

    update format, nav and json update

---
+++ logic.js
@@ -0,0 +1,64 @@
+// CODE CERTIFICATES
+
+function leerJson(ruta) {
+  return fetch(ruta)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("JSON not found");
+      }
+      return response.json();
+    })
+    .catch((error) => {
+      console.error(
+        "There has been a problem with your fetch operation:",
+        error
+      );
+    });
+}
+
+function mostrarDatos(datos) {
+  var divCursos = document.getElementById("cursos");
+  var cursos = datos.cursos;
+
+  console.log(cursos);
+  cursos.forEach((curso) => {
+    var card = document.createElement("div");
+    card.className = "card cert-card";
+    card.innerHTML = `
+          <div class="card-body">
+            <h5 class="card-title">${curso.title}</h5>
+            <p class="card-text">Issued by: <strong>${curso.description}</strong> </br> ${curso.duration}</p>
+            <a href="${curso.link}" class="btn btn-primary">View Certification</a>
+          </div>
+    `;
+    divCursos.appendChild(card);
+  });
+}
+
+function cargarDatos() {
+  leerJson("data/cursos.json")
+    .then((datos) => {
+      mostrarDatos(datos);
+    })
+    .catch((error) => {
+      console.error("Error al cargar los datos:", error);
+    });
+}
+
+cargarDatos();
+
+//FORM GMAIL
+imagen = document.getElementById("profile-picture");
+
+imagen.addEventListener("click", function () {
+  window.open(
+    "form.html",
+    "_blank",
+    "width=800,height=560",
+    "resizable=no",
+    "scrollbars=no",
+    "toolbar=no",
+    "location=no",
+    "status=no"
+  );
+});
